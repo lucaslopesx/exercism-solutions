5b770c858e4a6e9115beed7dbf8814ac771e4537
lucaslopesx <lucas.lopesx1@gmail.com>
2024-03-11 19:40:38 -0300

    scrabble_score.go second iteration

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,38 +1,22 @@
 package scrabble
 
 import (
-	"fmt"
-	"strings"
+	"unicode"
 )
- 
-
-func getLetterValue(letter rune) int {
-	c := strings.ToUpper(fmt.Sprintf("%c", letter))
-
-	switch c {
-	case "A", "E", "I", "O", "U", "L", "N", "R", "S", "T":
-		return 1
-  case "D", "G":
-		return 2
-	case "B", "C", "M", "P":
-		return 3
-	case "F", "H", "V", "W", "Y":
-		return 4
-	case "K":
-		return 5
-	case "J", "X":
-		return 8
-	case "Q", "Z":
-		return 10
-	}
-
-	return 0
+var	letterMap = map[rune]int{
+	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+	'D': 2, 'G': 2,
+	'B': 3, 'C': 3, 'M': 3, 'P': 3,
+	'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+	'K': 5,
+	'J': 8, 'X': 8,
+	'Q': 10, 'Z': 10,
 }
 
 func Score(word string) int {
 	var sum int = 0;
-	for _, v := range word {
-		sum += getLetterValue(v)
+	for _, c := range word {
+		sum += letterMap[unicode.ToUpper(c)]
 	}
 
 	return sum
