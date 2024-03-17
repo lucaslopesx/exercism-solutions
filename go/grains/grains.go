97345e23452952149c8075b136e98c43120b90fa
lucaslopesx <lucas.lopesx1@gmail.com>
2024-03-17 16:02:44 -0300

    Complete grains.go

---
+++ go/grains/grains.go
@@ -0,0 +1,15 @@
+package grains
+
+import "errors"
+
+func Square(number int) (uint64, error) {
+	if number <= 0 || number > 64 {
+		return 0, errors.New("negative or zero number not supported")
+	}
+
+	return (1 << (number - 1)), nil
+}
+
+func Total() uint64 {
+	return (1 << 64) - 1
+}
